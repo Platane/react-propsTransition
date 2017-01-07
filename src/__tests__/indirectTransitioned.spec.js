d980d72482fdbbdd5cf11db2919471927638712f
Platane <me@platane.me>
2017-01-07 13:07:27 +0100

    :rocket: test

---
+++ src/__tests__/indirectTransitioned.spec.js
@@ -0,0 +1,159 @@
+import React from 'react'
+import IndirectTransitioned from '../indirectTransitioned'
+import renderer from 'react-test-renderer'
+
+const DumbComponent = props =>
+  <IndirectTransitioned toTransition={props.a} delay={500} equal={(u, v) => u[1] === v[1]}>
+    {
+      ({ next, previous, transition }) =>
+        <div {...props}>{`b is ${props.b}; ` + (!transition ? `a is ${next}` : `a transition from ${previous} to ${next}`)}</div>
+    }
+  </IndirectTransitioned>
+
+class Component extends React.Component {
+
+  constructor (props) {
+    super(props)
+    this.state = { a: props.a, b: props.b }
+  }
+
+  render () {
+    return <DumbComponent {...this.state} setA={a => this.setState({a})} setB={b => this.setState({b})} />
+  }
+}
+
+describe('indirectTransitioned component', () => {
+  describe('iitial render', () => {
+    it('should render without transition', () => {
+      const component = renderer.create(<Component a={'A1'} b={'B1'} />)
+      let tree = component.toJSON()
+      expect(tree.children[0]).toEqual('b is B1; a is A1')
+    })
+  })
+
+  describe('set props not marked as to transition', () => {
+    it('should render without transition', () => {
+      const component = renderer.create(<Component a={'A1'} b={'B1'} />)
+      let tree = component.toJSON()
+
+      // set b
+      tree.props.setB('B2')
+      tree = component.toJSON()
+      expect(tree.children[0]).toEqual('b is B2; a is A1')
+    })
+  })
+
+  describe('set props marked as to transition', () => {
+    it('should render with transition', () => {
+      jest.useFakeTimers()
+
+      const component = renderer.create(<Component a={'A1'} b={'B1'} />)
+      let tree = component.toJSON()
+
+      // set a
+      tree.props.setA('A2')
+      tree = component.toJSON()
+      expect(tree.children[0]).toEqual('b is B1; a transition from A1 to null')
+      expect(setTimeout.mock.calls.length).toBe(1)
+      expect(setTimeout.mock.calls[0][1]).toBe(500)
+
+      // after a delay
+      jest.runOnlyPendingTimers()
+      tree = component.toJSON()
+      expect(tree.children[0]).toEqual('b is B1; a transition from null to A2')
+      expect(setTimeout.mock.calls.length).toBe(2)
+      expect(setTimeout.mock.calls[1][1]).toBe(500)
+
+      jest.runOnlyPendingTimers()
+      tree = component.toJSON()
+      expect(tree.children[0]).toEqual('b is B1; a is A2')
+      expect(setTimeout.mock.calls.length).toBe(2)
+    })
+  })
+
+  describe('set props marked as to transition to the same value', () => {
+    it('should render without transition', () => {
+      jest.useFakeTimers()
+
+      const component = renderer.create(<Component a={'A1'} b={'B1'} />)
+      let tree = component.toJSON()
+
+      // set a
+      tree.props.setA('A1')
+      tree = component.toJSON()
+      expect(tree.children[0]).toEqual('b is B1; a is A1')
+      expect(setTimeout.mock.calls.length).toBe(0)
+    })
+  })
+
+  describe('set props marked as to transition to a value equal to previous', () => {
+    it('should render without transition', () => {
+      jest.useFakeTimers()
+
+      const component = renderer.create(<Component a={'A1'} b={'B1'} />)
+      let tree = component.toJSON()
+
+      // set a
+      tree.props.setA('A1_')
+      tree = component.toJSON()
+      expect(tree.children[0]).toEqual('b is B1; a is A1')
+      expect(setTimeout.mock.calls.length).toBe(0)
+    })
+  })
+
+  describe('set props marked as to transition, set again during the transition to null', () => {
+    it('should render with transition', () => {
+      jest.useFakeTimers()
+
+      const component = renderer.create(<Component a={'A1'} b={'B1'} />)
+      let tree = component.toJSON()
+
+      // set a
+      tree.props.setA('A2')
+      tree = component.toJSON()
+      expect(tree.children[0]).toEqual('b is B1; a transition from A1 to null')
+
+      tree.props.setA('A3')
+      tree = component.toJSON()
+      expect(tree.children[0]).toEqual('b is B1; a transition from null to A3')
+
+      // after a delay
+      jest.runOnlyPendingTimers()
+      tree = component.toJSON()
+      expect(tree.children[0]).toEqual('b is B1; a is A3')
+    })
+  })
+
+  describe('set props marked as to transition, set again during the transition from null', () => {
+    it('should render with transition', () => {
+      jest.useFakeTimers()
+
+      const component = renderer.create(<Component a={'A1'} b={'B1'} />)
+      let tree = component.toJSON()
+
+      // set a
+      tree.props.setA('A2')
+      tree = component.toJSON()
+      expect(tree.children[0]).toEqual('b is B1; a transition from A1 to null')
+
+      jest.runOnlyPendingTimers()
+
+      tree = component.toJSON()
+      expect(tree.children[0]).toEqual('b is B1; a transition from null to A2')
+
+      tree.props.setA('A3')
+      tree = component.toJSON()
+      expect(tree.children[0]).toEqual('b is B1; a transition from A2 to null')
+
+      jest.runOnlyPendingTimers()
+
+      tree = component.toJSON()
+      expect(tree.children[0]).toEqual('b is B1; a transition from null to A3')
+
+      // after a delay
+      jest.runOnlyPendingTimers()
+      tree = component.toJSON()
+      expect(tree.children[0]).toEqual('b is B1; a is A3')
+    })
+  })
+})
---
+++ src/__tests__/transitioned.spec.js
@@ -0,0 +1,120 @@
+import React from 'react'
+import Transitioned from '../transitioned'
+import renderer from 'react-test-renderer'
+
+const DumbComponent = props =>
+  <Transitioned toTransition={props.a} delay={500} equal={(u, v) => u[1] === v[1]}>
+    {
+      ({ next, previous, transition }) =>
+        <div {...props}>{`b is ${props.b}; ` + (!transition ? `a is ${next}` : `a transition from ${previous} to ${next}`)}</div>
+    }
+  </Transitioned>
+
+class Component extends React.Component {
+
+  constructor (props) {
+    super(props)
+    this.state = { a: props.a, b: props.b }
+  }
+
+  render () {
+    return <DumbComponent {...this.state} setA={a => this.setState({a})} setB={b => this.setState({b})} />
+  }
+}
+
+describe('transitioned component', () => {
+  describe('iitial render', () => {
+    it('should render without transition', () => {
+      const component = renderer.create(<Component a={'A1'} b={'B1'} />)
+      let tree = component.toJSON()
+      expect(tree.children[0]).toEqual('b is B1; a is A1')
+    })
+  })
+
+  describe('set props not marked as to transition', () => {
+    it('should render without transition', () => {
+      const component = renderer.create(<Component a={'A1'} b={'B1'} />)
+      let tree = component.toJSON()
+
+      // set b
+      tree.props.setB('B2')
+      tree = component.toJSON()
+      expect(tree.children[0]).toEqual('b is B2; a is A1')
+    })
+  })
+
+  describe('set props marked as to transition', () => {
+    it('should render with transition', () => {
+      jest.useFakeTimers()
+
+      const component = renderer.create(<Component a={'A1'} b={'B1'} />)
+      let tree = component.toJSON()
+
+      // set a
+      tree.props.setA('A2')
+      tree = component.toJSON()
+      expect(tree.children[0]).toEqual('b is B1; a transition from A1 to A2')
+      expect(setTimeout.mock.calls.length).toBe(1)
+      expect(setTimeout.mock.calls[0][1]).toBe(500)
+
+      // after a delay
+      jest.runOnlyPendingTimers()
+      tree = component.toJSON()
+      expect(tree.children[0]).toEqual('b is B1; a is A2')
+      expect(setTimeout.mock.calls.length).toBe(1)
+    })
+  })
+
+  describe('set props marked as to transition to the same value', () => {
+    it('should render without transition', () => {
+      jest.useFakeTimers()
+
+      const component = renderer.create(<Component a={'A1'} b={'B1'} />)
+      let tree = component.toJSON()
+
+      // set a
+      tree.props.setA('A1')
+      tree = component.toJSON()
+      expect(tree.children[0]).toEqual('b is B1; a is A1')
+      expect(setTimeout.mock.calls.length).toBe(0)
+    })
+  })
+
+  describe('set props marked as to transition to a value equal to previous', () => {
+    it('should render without transition', () => {
+      jest.useFakeTimers()
+
+      const component = renderer.create(<Component a={'A1'} b={'B1'} />)
+      let tree = component.toJSON()
+
+      // set a
+      tree.props.setA('A1_')
+      tree = component.toJSON()
+      expect(tree.children[0]).toEqual('b is B1; a is A1')
+      expect(setTimeout.mock.calls.length).toBe(0)
+    })
+  })
+
+  describe('set props marked as to transition, set again during the transition delay', () => {
+    it('should render with transition', () => {
+      jest.useFakeTimers()
+
+      const component = renderer.create(<Component a={'A1'} b={'B1'} />)
+      let tree = component.toJSON()
+
+      // set a
+      tree.props.setA('A2')
+      tree = component.toJSON()
+      expect(tree.children[0]).toEqual('b is B1; a transition from A1 to A2')
+
+      tree.props.setA('A3')
+      tree = component.toJSON()
+      expect(tree.children[0]).toEqual('b is B1; a transition from A2 to A3')
+
+      // after a delay
+      jest.runOnlyPendingTimers()
+      tree = component.toJSON()
+      expect(tree.children[0]).toEqual('b is B1; a is A3')
+    })
+  })
+})
